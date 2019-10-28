5d706103d1e06dcbbce9c4241f6b574e544a3c97
Student <example@example.com>
2019-10-28 14:23:21 -0700

    create webpage and add looping without writing the flavors in html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var flavors = ['vanilla', 'chocolate', 'strawberry', 'banaba'];
+  flavors.forEach(function(flavor) {
+    alert('I love ' + flavor + '.')
+  });
+});
