c5384515466e2b4b95bc21d1382a8fee9dd01a1a
Student <example@example.com>
2019-10-29 11:48:50 -0700

    change forEach() looping to for looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   var flavors = ['vanilla', 'chocolate', 'strawberry', 'banaba'];
-  flavors.forEach(function(flavor) {
+
+  for (var i = 0; i < flavors.length; i += 1) {
+    alert('I love ' + flavors[i] + '.')
+  }
+/*  flavors.forEach(function(flavor) {
     alert('I love ' + flavor + '.')
-  });
+  }); */
 });
